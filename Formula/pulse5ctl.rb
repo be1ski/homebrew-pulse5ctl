c44f96001999b906386fb84cfc39431524f5e506
Dmitriy Belskiy <dmitry.belskiy@aviasales.ru>
2026-02-22 22:15:59 +0700

    pulse5ctl 1.12.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pulse5ctl.rb
+++ Formula/pulse5ctl.rb
@@ -1,8 +1,8 @@
 class Pulse5ctl < Formula
   desc "Pulse 5 speaker controller — macOS menu bar app"
   homepage "https://github.com/be1ski/pulse5ctl"
-  url "https://github.com/be1ski/pulse5ctl/archive/refs/tags/v1.11.1.tar.gz"
-  sha256 "116e05586984d52dfcd7fe2977f8c69c938b97c50cfeb491091cdd9f0233d029"
+  url "https://github.com/be1ski/pulse5ctl/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "8cb637cc4e7fcff71519ab200053c37fb99862bcd518b05a8434321f3712a9ef"
   license "MIT"
 
   depends_on :macos
